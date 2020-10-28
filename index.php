228d0573e95c5cc47febfab5d29bf3e22288b0c2
4upakabra199 <47057098+4upakabra199@users.noreply.github.com>
2020-10-28 12:31:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
   <html lang="ru">
-  <head>
-    <title><Корпоратив Мессенджер></title>
-  </head>
+  
   <body>
     <header class="main-header">
       <h1 class="visually-hidden">До корпоратива осталось:</h1>
